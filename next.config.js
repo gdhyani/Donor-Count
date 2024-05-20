c17a356fe7620f8501180d211f4fb399ac7cd13d
Gaurav Dhyani <gauravdhyani90@gmail.com>
2024-05-20 23:41:49 +0530

    project done

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        net: false,
+        tls: false,
+        // other fallbacks can be added here if necessary
+      }
+    }
+    return config
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
